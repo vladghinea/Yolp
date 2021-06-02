9f2c842c42f39e0a9a31b41ea9055752d019a566
keitkalon <keitkalon@gmail.com>
2021-06-02 22:50:04 +0300

    comment css design

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 vlad = '/home/vlad/projects/ask-mate-2-python-vladghinea/static/images/uploads/'
 lamine = '/home/keitkalon/projects/web/ask-mate-2-python-vladghinea/static/images/uploads'
 home = os.path.join(app.root_path, 'static/images/uploads')
-app.config['IMAGE_UPLOADS'] = vlad
+app.config['IMAGE_UPLOADS'] = lamine
 app.config['ALLOWED_IMAGE_EXTENSION'] = ['PNG', 'JPG', 'JPEG']
 
 @app.route("/")
